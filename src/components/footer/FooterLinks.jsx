96333f9730d1162e739e50f1a0cc6655fe604978
User-XYZ <nwosu4686@gmail.com>
2023-06-08 01:58:29 +0100

    modified Footer components

--- src/components/footer/FooterLinks.jsx
+++ src/components/footer/FooterLinks.jsx
@@ -6,7 +6,7 @@ const FooterLinks = () => {
         <div key={title}>
             <h6 className='font-medium text-gray-400'>{title}</h6>
             <ul>
-                {links.map(( value ) => <li key={value} className='py-2 text-sm'>{value}</li> )}
+                {links.map(( value ) => <li key={value} className='py-2 text-sm hover:text-gray-600 cursor-pointer'>{value}</li> )}
             </ul>
         </div>
       )
